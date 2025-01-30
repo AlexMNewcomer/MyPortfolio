e22a466853064f6dd6c5532e10308d1c7dba1ad8
Alex Newcomer <alexmnewcomer@gmail.com>
2025-01-30 10:25:49 -0500

    Defined the Portfolio

--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import App from './App';
-import Error from './pages/Portfolio';
+import Portfolio from './pages/Portfolio';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import AboutMe from './pages/AboutMe';
