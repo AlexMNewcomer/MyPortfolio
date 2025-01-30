9580eae5c8854abc6a0724700f16b37a727d0e87
Alex Newcomer <alexmnewcomer@gmail.com>
2025-01-30 11:38:04 -0500

    Updated the Home page

--- Portfolio/src/pages/Home.jsx
+++ Portfolio/src/pages/Home.jsx
@@ -3,16 +3,20 @@ export default function Home() {
     <div>
       <h1>Home Page</h1>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+      I am a motivated security coordinator & general warehouse safety trainer at Saadia Distribution as well as a sales lead/trainer for Herr's Fruit Farm with 4+ years of experience in security & 10+ years of experience in sales & inventory management. Since these fields are quite different from each other, they've allowed me to gather different viewpoints & knowledge to enhance my abilities in a multitude of aspects. I'm also currently studying software development at Rutgers University and will be graduating in April. Not only that, they've allowed me to recognize what I'm passionate about & continuously striving for which is leading an organized & confident team to reach maximum effectiveness & proficiency for an organization while maintaining the highest possible team member satisfaction.
+
+Some examples of my results include;
+
+QVC:
+- Developed a more adept onboarding process for potential candidates to maximize safety while maintaining high levels of efficiency.
+- Constituted clear & detailed step-by-step guides on how to both properly enter new team members into our GEPP System & create scannable badges for each new member as well.
+
+Herr's:
+- Continuously set new sales records during our most important holiday events over the last 3 years & set a new record in sales over our summer season in 2021.
+
+I love learning. I currently have an associate degree in Business Administration from HACC. However, I do plan on continuing my education & obtaining a bachelors degree in Business Management from Millersville University. I also have every intention in becoming fluent in Spanish. 
+
+Outside of work, I spend a lot of time at the gym trying to maintain a healthy body. When I'm not at the gym or at work, my favorite hobby is playing & collecting a strategy card game called Magic the Gathering. I absolutely love the creativity, complex strategies, problem solving & teamwork that comes from playing. 
       </p>
     </div>
   );
